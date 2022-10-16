5ff0b340e83e6fa1b7f0f59f521838f002e6fc3e
Alfred Langerbeck <98alla02@gmail.com>
2022-10-16 23:23:51 +0200

    Started on chapter 4

---
+++ ch-3/src/main.rs
@@ -0,0 +1,34 @@
+use std::io;
+
+fn mutablexample() {
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+}
+
+fn invalid_array_element() {
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
+}
+
+fn main() {
+    //mutablexample();
+    invalid_array_element();
+}
